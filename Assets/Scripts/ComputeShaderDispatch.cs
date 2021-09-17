4ece2b0a37794d0a0cdbfdc598e9adc43e3a1a7e
Axel Nyström Svensson <axel.nystrom-svensson@hotmail.com>
2021-09-17 14:43:21 +0200

    Normals in particle shader, refactoring of ComputeShaderDispatch

--- Assets/Scripts/ComputeShaderDispatch.cs
+++ Assets/Scripts/ComputeShaderDispatch.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public struct particle{
     public float x;
@@ -14,27 +15,22 @@ public class ComputeShaderDispatch : MonoBehaviour{
     [SerializeField] private ComputeShader shader;
 
     //Buffers to store our particle data.
-    [SerializeField] private particle[] particles;   //On the CPU
-    ComputeBuffer particleBuffer;  //On the GPU
+    [SerializeField] private particle[] particles;  //On the CPU
+    ComputeBuffer particleBuffer;                   //On the GPU
 
-    private GameObject[] particlesObjects;
-
-    [SerializeField] private int particleCount = 1024;
     private int CSMainKernel;
 
-    [Header("Draw Procedural")]
-    [SerializeField] Material material;
-    [SerializeField] Mesh mesh;
+    [Header("Particles")]
+    [SerializeField] Material particleMaterial;
+    [SerializeField] Mesh particleMesh;
 
-    // ComputeBuffer resultBuffer;
+    [Range(1, 65534)]
+    [SerializeField] private int particleCount = 1024;
 
-    //Contains triangle data for the particle mesh.
+    //Buffers that contain data about our particle mesh.
     ComputeBuffer meshTriangles;
-    //Contains vertices data for the particle mesh.
     ComputeBuffer meshVertices;
-
-    Vector3[] vertices;
-    int[] triangles;
+    ComputeBuffer meshNormals;
 
     void Start(){
         if(shader == null){
@@ -43,26 +39,40 @@ public class ComputeShaderDispatch : MonoBehaviour{
         }
         particles = new particle[particleCount];
 
+        InitializeBuffers();
+    }
+
+    private void InitializeBuffers(){
+        CSMainKernel = shader.FindKernel("CSMain");
 
         //ComputeBuffer(count, stride) (number of elements, size of one element)
         //Here we're creating a compute buffer with particles length, each particle struct contains 4 floats (each float being 4 bytes).
         particleBuffer = new ComputeBuffer(particles.Length, sizeof(float)*4);
         particleBuffer.SetData(particles);
-
-        CSMainKernel = shader.FindKernel("CSMain");
         shader.SetBuffer(CSMainKernel, "particleBuffer", particleBuffer);
-        material.SetBuffer("particles", particleBuffer);
+        particleMaterial.SetBuffer("particles", particleBuffer);
 
-        vertices = mesh.vertices;
+        Vector3[] vertices = particleMesh.vertices;
         meshVertices = new ComputeBuffer(vertices.Length, sizeof(float) * 3);
         meshVertices.SetData(vertices);
-        material.SetBuffer("vertices", meshVertices);
+        particleMaterial.SetBuffer("vertices", meshVertices);
 
-        triangles = mesh.triangles;
+        int[] triangles = particleMesh.triangles;
         meshTriangles = new ComputeBuffer(triangles.Length, sizeof(int));
         meshTriangles.SetData(triangles);
-        material.SetBuffer("triangles", meshTriangles);
+        particleMaterial.SetBuffer("triangles", meshTriangles);
 
+        Vector3[] normals = particleMesh.normals;
+        meshNormals = new ComputeBuffer(normals.Length, sizeof(float) * 3);
+        meshNormals.SetData(normals);
+        particleMaterial.SetBuffer("normals", meshNormals);
+    }
+
+    private void DisposeBuffers(){
+        particleBuffer.Dispose();
+        meshTriangles.Dispose();
+        meshVertices.Dispose();
+        meshNormals.Dispose();
     }
 
     void Update(){
@@ -71,22 +81,21 @@ public class ComputeShaderDispatch : MonoBehaviour{
         //Keep in mind that this is multiplied with the numthreads in the actual shader file.
         //The total number of threads must be more than the number of particles in our simulation, since
         //we're directly manipulating only one particle per thread.
-        shader.Dispatch(CSMainKernel, 32, 1, 1);
+        shader.Dispatch(CSMainKernel, particleCount, 1, 1);
         
         //Instead of actually having game objects represent each particle, we just take the results from 
         //our compute shader and directly renders that data.
         //https://forum.unity.com/threads/most-efficient-way-to-render-particle-sprites.487496/
-        Graphics.DrawProcedural(material, new Bounds(new Vector3(0,0,0), new Vector3(80,80,80)), MeshTopology.Triangles, meshTriangles.count, 1024);
-
-        //DEBUG!!
-        particleBuffer.GetData(particles);
-        Debug.Log(string.Format("({0}, {1}, {2})", particles[0].x, particles[0].y, particles[0].z));
-        
+        Graphics.DrawProcedural(material: particleMaterial, 
+                                bounds: new Bounds(new Vector3(0,0,0), new Vector3(80,80,80)), 
+                                topology: MeshTopology.Triangles, 
+                                vertexCount:meshTriangles.count, 
+                                instanceCount: particleCount, 
+                                castShadows: ShadowCastingMode.Off, 
+                                receiveShadows: false);
     }
 
     void OnDestroy(){
-        particleBuffer.Dispose();
-        meshTriangles.Dispose();
-        meshVertices.Dispose();
+        DisposeBuffers();
     }
 }
---
+++ Assets/Shaders/ParticleShader.shader
@@ -0,0 +1,66 @@
+//Shader is originally based on:
+//https://www.ronja-tutorials.com/post/051-draw-procedural/
+
+Shader "Unlit/ParticleShader"
+{
+    Properties{
+        [HDR] _Color ("Tint", Color) = (0, 0, 0, 1)
+        _LightDirection ("Light Direction", Vector) = (0, 0.707106, 0.707106)
+    }
+    SubShader{
+        Tags{ "RenderType"="Opaque" "Queue"="Geometry" }
+
+        Pass{
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            //tint of the texture
+            fixed4 _Color;
+            float3 _LightDirection;
+
+            struct particle{
+                float x;
+                float y;
+                float z;
+                float dummy;
+            };
+
+            struct v2f {
+                float4 pos : SV_POSITION;
+                fixed4 color : COLOR;
+            };
+
+            //buffers
+            StructuredBuffer<particle> particles;
+            StructuredBuffer<int> triangles;
+            StructuredBuffer<float3> vertices;
+            StructuredBuffer<float3> normals;
+
+            //the vertex shader function
+            v2f vert(uint vertex_id: SV_VertexID, uint instance_id: SV_InstanceID){
+                v2f o;
+                int positionIndex = triangles[vertex_id];
+                float3 position = vertices[positionIndex];
+                //add sphere position
+                position += float3(particles[instance_id].x, particles[instance_id].y, particles[instance_id].z);
+                //convert the vertex position from world space to clip space
+                o.pos = mul(UNITY_MATRIX_VP, float4(position, 1));
+                o.color = float4(normals[positionIndex], 1);
+                return o;
+            }
+
+            //the fragment shader function
+            fixed4 frag(v2f i) : SV_TARGET{
+                //return the final color to be drawn on screen
+                float4 finalColor = dot(normalize(i.color), normalize(_LightDirection)) * _Color;
+                return finalColor;
+            }
+
+            ENDCG
+            }   
+        }
+        Fallback "VertexLit"
+}
